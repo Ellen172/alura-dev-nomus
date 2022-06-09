579e750b2ca1f2f563e534e9a161672bfff481d3
ellen-ubuntu <ellencas172@gmail.com>
2022-06-09 15:46:42 -0300

    refatoração

---
+++ RefatoracaoBanco/src/Conta.java
@@ -0,0 +1,25 @@
+
+
+public class Conta {
+	private double saldo;
+	
+	public Conta(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public void depositar(double valor) {
+		this.saldo = this.saldo+valor;
+	}
+	
+	public void sacar(double valor) {
+		this.saldo = this.saldo-valor;
+	}
+	
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public double getSaldo() {
+		return saldo;
+	}
+}
