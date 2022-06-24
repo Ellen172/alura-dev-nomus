992b11ddb2e30ba0c365e52b7047bc0d94ca4051
ellen-ubuntu <ellencas172@gmail.com>
2022-06-24 12:55:47 -0300

    realizando consultas no bd

--- Persistencia_JPA/loja/src/main/java/br/com/alura/loja/dao/ProdutoDao.java
+++ Persistencia_JPA/loja/src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -1,5 +1,7 @@
 package br.com.alura.loja.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import br.com.alura.loja.modelo.Produto;
@@ -12,9 +14,27 @@ public class ProdutoDao {
 		super();
 		this.em = em;
 	}
-	
+
 	public void cadastrar(Produto produto) {
-		this.em.persist(produto); //cadastra produto, insere no bd
+		this.em.persist(produto); // cadastra produto, insere no bd
 	}
 
+	public void atualizar(Produto produto) {
+		this.em.merge(produto);
+	}
+
+	public void remover(Produto produto) {
+		produto = this.em.merge(produto);
+		this.em.remove(produto);
+	}
+	
+	public Produto buscarPorId(Long id) {
+		return em.find(Produto.class, id); //retorna o elemento da tabela 'Produto.class' com id 'id'
+	}
+
+	public List<Produto> buscarTodos(){
+		String jpql = "SELECT p FROM Produto p"; // no SQL -> "SELECT * FROM produtos"
+		//return em.createQuery(jpql).getResultList(); // retorna o resultado da query
+		return em.createQuery(jpql, Produto.class).getResultList(); //informa o jpql e a classe que será buscada
+	}
 }
--- Persistencia_JPA/loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ Persistencia_JPA/loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,6 +1,7 @@
 package br.com.alura.loja.testes;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 
@@ -12,38 +13,50 @@ import br.com.alura.loja.util.JPAUtil;
 
 public class CadastroDeProduto {
 
-	public static void main(String[] args) {
-		
-		
-		Categoria celulares = new Categoria("CELULARES");
-		//Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
+	public static void main(String[] args) {		
+		cadastrarProdutoTeste();
 		
 		EntityManager em = JPAUtil.getEntityManager();
-		//CategoriaDao categoriaDao = new CategoriaDao(em);
-		//ProdutoDao produtoDao = new ProdutoDao(em);
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		
+		Produto p1 = produtoDao.buscarPorId(1l); //select de um elemento da tabela
+		System.out.println(p1.getPreco());
+		
+		List<Produto> todos = produtoDao.buscarTodos(); //select de todos os elementos de uma tabela
+		todos.forEach(p -> System.out.println(p.getNome()));
+	}
+
+	private static void cadastrarProdutoTeste() {
+		Categoria celulares = new Categoria("CELULARES");
+		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
+		Produto celular2 = new Produto("LG K11+", "Celular lg", new BigDecimal("700"), celulares);
+		
+		EntityManager em = JPAUtil.getEntityManager();
+		CategoriaDao categoriaDao = new CategoriaDao(em);
+		ProdutoDao produtoDao = new ProdutoDao(em);
 		
 		em.getTransaction().begin(); //inicia transação
 		
-		//categoriaDao.cadastrar(celulares); //salva a categoria celulares no bd
-		//produtoDao.cadastrar(celular); //salva produto celular no bd
+		//create - insert 
+		categoriaDao.cadastrar(celulares);
+		produtoDao.cadastrar(celular); 
+		produtoDao.cadastrar(celular2);
 		
-		em.persist(celulares); //envia do estado transient para o estado managed
+		//em.persist(celulares); //envia do estado transient para o estado managed
 		
-		celulares.setNome("XTP"); //update nome
-		em.flush(); //envia do estado managed para o banco, mas não finaliza
-		em.clear(); //limpa o manager, envia para o estado detached
+		//celulares.setNome("XTP"); //update nome
+		//em.flush(); //envia do estado managed para o banco, mas não finaliza
+		//em.clear(); //limpa o manager, envia para o estado detached
 				
-		celulares = em.merge(celulares); //busca no estado detached, envia para managed
-		celulares.setNome("ABC"); //update nome
-		em.flush(); // envia para o banco
+		//celulares = em.merge(celulares); //busca no estado detached, envia para managed
+		//celulares.setNome("ABC"); //update nome
+		//em.flush(); // envia para o banco
 		//em.clear(); //dá erro, pois não será possivel remover da classe detached
-		em.remove(celulares); //deleta no banco, buscando no managed
-		em.flush();
+		//em.remove(celulares); //deleta no banco, buscando no managed
+		//em.flush();
 
-		//em.getTransaction().commit(); //finaliza transcação, envia para o banco
+		em.getTransaction().commit(); //finaliza transcação, envia para o banco
 		em.close(); //finaliza entity manager, limpa detached
-		
-		 
 	}
 
 }
