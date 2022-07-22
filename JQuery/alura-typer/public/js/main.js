5d660a88760a00675400dc0a80265eda8e61434c
Ellen172 <ellencas172@gmail.com>
2022-07-22 15:04:53 -0300

    reiniciando jogo

--- JQuery/alura-typer/public/js/main.js
+++ JQuery/alura-typer/public/js/main.js
@@ -1,32 +1,65 @@
+// $(".frase") = jQuery(".frase") 
 
-/* $(".frase") = jQuery(".frase") */
-var frase = $(".frase").text(); //retorna o texto do elemento que tem classe frase
-var tamanhoFrase = $("#tamanho-frase");
-contaPalavras(frase, tamanhoFrase);
-
-// contadores do textarea
 var campoDigitacao = $("#campo-digitacao");
-var contPalavras = $("#contador-palavras");
-var contCaracteres = $("#contador-caracteres");
-campoDigitacao.on("input", function(){
-    contaPalavras(campoDigitacao.val(), contPalavras); //conta as palavras
-    contCaracteres.text(campoDigitacao.val().length); //conta os caracteres (tamanho total)
+var tempoInicial = $("#tempo-digitacao").text();
+var cronometro = $("#tempo-digitacao");
+
+$(function(){
+//$(document).ready(function(){
+
+    //executado assim que a pagina estiver pronta
+    console.log(tempoInicial);
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro(tempoInicial);
+        
+    $("#btn-reiniciar").click(reiniciaJogo);
 });
 
-// decrescer contador de tempo
-var tempoRestante = $("#tempo-digitacao").text();
-campoDigitacao.one("focus", function(){ // chama na primeira vez que ocorre o focus
-    var cronometroId = setInterval(function(){
-        tempoRestante--;
-        $("#tempo-digitacao").text(tempoRestante); 
-        console.log(tempoRestante);
-        if(tempoRestante < 1){
-            // game over
-            campoDigitacao.attr("disabled", "");
-            clearInterval(cronometroId);
-        }
-    }, 1000);
-});
+
+/* funções */  
+
+
+function reiniciaJogo(){
+    campoDigitacao.attr("disabled", false); //habilita textarea
+    campoDigitacao.val(""); //reseta valor do campo
+
+    inicializaContadores();
+    cronometro.text(tempoInicial);
+    inicializaCronometro(tempoInicial);
+}
+
+function inicializaCronometro(tempoRestante){
+    campoDigitacao.one("focus", function(){ // chama na primeira vez que ocorre o focus
+        var cronometroId = setInterval(function(){
+            tempoRestante--;
+            cronometro.text(tempoRestante); 
+            
+            if(tempoRestante < 1){
+                // game over
+                campoDigitacao.attr("disabled", true);
+                clearInterval(cronometroId);
+            }
+        }, 1000);
+    });
+}
+
+function inicializaContadores(){
+    campoDigitacao.on("input", function(){        
+        var contPalavras = $("#contador-palavras");
+        var contCaracteres = $("#contador-caracteres");
+        
+        contaPalavras(campoDigitacao.val(), contPalavras); //conta as palavras
+        contCaracteres.text(campoDigitacao.val().length); //conta os caracteres (tamanho total)
+    });
+}
+
+function atualizaTamanhoFrase(){
+    var frase = $(".frase").text(); //retorna o texto do elemento que tem classe frase
+    var tamanhoFrase = $("#tamanho-frase");
+    contaPalavras(frase, tamanhoFrase);
+    
+}
 
 function contaPalavras(frase, elemento){
     var numPalavras = frase.split(/\S+/).length -1; //cria uma array, onde cada espaço vazio divide o elemento e retorna o tamanho
