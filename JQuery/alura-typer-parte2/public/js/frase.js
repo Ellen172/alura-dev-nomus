82d4637a9337d16034dcd170bef90ea246e28bdd
Ellen172 <ellencas172@gmail.com>
2022-07-27 14:32:49 -0300

    tratando erro do get

--- JQuery/alura-typer-parte2/public/js/frase.js
+++ JQuery/alura-typer-parte2/public/js/frase.js
@@ -1,12 +1,20 @@
 $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria(){
-    $.get("http://localhost:3000/frases", trocaFraseAleatoria);
+    $.get("http://localhost:3000/frases", trocaFraseAleatoria)
+        .fail(function(){
+            $("#erro").show();
+            setTimeout(() => {
+                $("#erro").toggle();
+            }, 2000);
+        });
+
 }
 
 function trocaFraseAleatoria(data){
     var frase = $(".frase");
     var numAleatorio = Math.floor(Math.random() * data.length);
-    console.log(numAleatorio);
     frase.text(data[numAleatorio].texto);
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data[numAleatorio].tempo);
 }
