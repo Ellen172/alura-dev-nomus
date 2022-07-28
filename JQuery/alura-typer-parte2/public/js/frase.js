b56029fda0b07301bfcd86e2154386d1ef4df081
Ellen172 <ellencas172@gmail.com>
2022-07-28 17:32:44 -0300

    busca com get

--- JQuery/alura-typer-parte2/public/js/frase.js
+++ JQuery/alura-typer-parte2/public/js/frase.js
@@ -1,4 +1,5 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-buscar-frase").click(buscaFrase);
 
 function fraseAleatoria(){
     $("#spinner").show();
@@ -22,4 +23,30 @@ function trocaFraseAleatoria(data){
     frase.text(data[numAleatorio].texto);
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numAleatorio].tempo);
+}
+
+function buscaFrase(){
+    $("#spinner").show();
+
+    var fraseId = $("#busca-frase").val();
+    console.log(fraseId);
+    var dados = { id: fraseId };
+    $.get("http://localhost:3000/frases", dados, trocaFraseId)
+        .fail(function(){
+            $("#erro").show();
+            setTimeout(() => {
+                $("#erro").toggle();
+            }, 2000);
+        })
+        .always(function(){
+            $("#spinner").toggle();
+        });
+}
+
+function trocaFraseId(data){
+    console.log(data);
+    var frase = $(".frase");
+    frase.text(data.texto);
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
 }
